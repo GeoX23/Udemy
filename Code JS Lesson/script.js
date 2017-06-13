5ff96c84fc4e604249648452b6d2da1f47e43933
GeoX <geox23.work@gmail.com>
2017-06-13 11:41:07 +0300

    Learning Code

---
+++ Code JS Lesson/script.js
@@ -0,0 +1,26 @@
+
+document.getElementById("myButton").addEventListener("click",function(){
+   myCar();
+   console.log(myObj1);
+})
+
+document.getElementById("myButton1").addEventListener("click",function(){
+   showMyCar();
+   
+})
+
+var myObj1 = new Object();
+function myCar(){
+myObj1.Car = prompt("What brand is your car ?");
+myObj1.Year = prompt("What year ?");
+myObj1.Model = prompt("What model ?");
+myObj1.Cost = prompt("How much did it cost ?");
+}
+
+function showMyCar(){
+    alert("My car's brand is : " +myObj1.Car);
+    alert("My car's year is : " +myObj1.Year);
+    alert("My car's model is : " +myObj1.Model);
+    alert("My car cost me  : " +myObj1.Cost);
+}
+
