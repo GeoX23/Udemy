b6ce4ab8ef0fb19112d95e50f9e5ca5a6384c93b
GeoX <geox23.work@gmail.com>
2017-07-20 12:19:51 +0300

    End of Lesson Code JS Lesson

--- Code JS Lesson/script.js
+++ Code JS Lesson/script.js
@@ -2,7 +2,7 @@ console.dir(document);
 
 // Variables
 
-var myEl = document.querySelectorAll (" li ");
+var myEl = document.querySelectorAll(" li ");
 var myOutput = document.getElementById("output");
 var myBtn = document.getElementById("myBtn");
 var myObj1 = new Object();
@@ -12,7 +12,7 @@ var output1 = document.getElementById("output1");
 
 
 
-myBtn.addEventListener("click",ChangeText);
+myBtn.addEventListener("click", ChangeText);
 
 
 //console Logs
@@ -20,68 +20,96 @@ myBtn.addEventListener("click",ChangeText);
 console.log(myEl);
 console.dir(myOutput);
 
-document.getElementById("myButton").addEventListener("click",function(){
-   myCar();
-   console.log(myObj1);
+document.getElementById("myButton").addEventListener("click", function () {
+    myCar();
+    console.log(myObj1);
 })
 
-document.getElementById("myButton1").addEventListener("click",function(){
-   showMyCar();
-   
-   
+document.getElementById("myButton1").addEventListener("click", function () {
+    showMyCar();
+
+
 })
 
-document.getElementById("updatebtn").onclick = function() {
+
+// Conditional Statement
+document.getElementById("updatebtn").onclick = function () {
     var myVal = document.getElementById("update").value;
-    if (myVal == "red"){
+    if (myVal == "red") {
         myVal = "Wow red is my fav color :D ";
     }
-    else  if (myVal == "blue"){
+    else if (myVal == "blue") {
         myVal = "Wow its blue";
     }
     else (
         myVal = "Not red or blue ... such a pitty :( "
     )
-    
+
     output1.innerHTML = myVal;
 }
 
 
 // loops
 
-for (i=0;i<myEl.length;i++){
-    myEl[i].addEventListener("click",myFun);
+for (i = 0; i < myEl.length; i++) {
+    myEl[i].addEventListener("click", myFun);
 }
 
 // functions
 
-function myCar(){
-myObj1.Car = prompt("What brand is your car ?");
-myObj1.Year = prompt("What year ?");
-myObj1.Model = prompt("What model ?");
-myObj1.Cost = prompt("How much did it cost ?");
+function myCar() {
+    myObj1.Car = prompt("What brand is your car ?");
+    myObj1.Year = prompt("What year ?");
+    myObj1.Model = prompt("What model ?");
+    myObj1.Cost = prompt("How much did it cost ?");
 }
 
-function showMyCar(){
-    alert("My car's brand is: " +myObj1.Car);
-    alert("My car's year is : " +myObj1.Year);
-    alert("My car's model is : " +myObj1.Model);
-    alert("My car cost me  : " +myObj1.Cost);
-} 
+function showMyCar() {
+    alert("My car's brand is: " + myObj1.Car);
+    alert("My car's year is : " + myObj1.Year);
+    alert("My car's model is : " + myObj1.Model);
+    alert("My car cost me  : " + myObj1.Cost);
+}
 
 
-function ChangeText(){
+function ChangeText() {
     myOutput.innerHTML = "You really need a new car !! ";
     myOutput.style.backgroundColor = "black";
-    myOutput.style.color = "red"; 
+    myOutput.style.color = "red";
     myEle.appendChild(myContent);
     myOutput.appendChild(myEle);
 }
 
-function myFun(){
+function myFun() {
     this.style.backgroundColor = "pink";
 }
 
+
+// Lesson Ajax - JSON 43.
+
+var jsonoutput = document.getElementById("jsonoutput");
+var jsonbtn = document.getElementById("jsonupdate");
+
+jsonbtn.onclick = function () {
+    var a = new XMLHttpRequest();
+
+    a.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            var l = JSON.parse(this.responseText);
+
+            for (o in l) {
+
+                jsonoutput.innerHTML += l[o].id + " " + l[o].title + "<br>";
+
+            }
+        }
+    }
+    a.open("GET", "http://jsonplaceholder.typicode.com/posts?userId=1", true);
+    a.send();
+
+
+
+};
 
 
 
